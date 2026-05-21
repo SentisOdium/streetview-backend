6879c0fad5f563e8ffc6bfc63669581ca602c6d9
Sentis Odium <jomaracecarmona@gmail.com>
2026-05-21 23:53:10 +0800

    feat: added an in-memory cache via apicache

--- routes/nodeRoutes/HotspotRoute.js
+++ routes/nodeRoutes/HotspotRoute.js
@@ -1,12 +1,27 @@
 import {Router} from 'express';
+import apicache from "apicache";
 import { fetchHotspotController } from '../../controllers/nodeController/nodeController.js';
 import { searchNodeController } from '../../controllers/nodeController/searchNodeController.js';
 import { pathGenController } from '../../controllers/nodeController/pathGenController.js';
 import nodeListController from '../../controllers/nodeController/nodeListController.js';
-const hotspotRouter = Router();
 
-hotspotRouter.get('/hotspots/:id', fetchHotspotController);
-hotspotRouter.get('/search', searchNodeController);
-hotspotRouter.get('/route', pathGenController);
-hotspotRouter.get('/list', nodeListController );
+const hotspotRouter = Router();
+const cache = apicache.middleware;
+
+hotspotRouter.get('/hotspots/:id', 
+        cache('60 minutes'), 
+            fetchHotspotController);
+
+hotspotRouter.get('/search', 
+        cache('30 minutes'), 
+            searchNodeController);
+
+hotspotRouter.get('/route', 
+        cache('30 minutes'), 
+            pathGenController);
+
+hotspotRouter.get('/list', 
+        cache('15 minutes'), 
+            nodeListController);
+
 export default hotspotRouter;
