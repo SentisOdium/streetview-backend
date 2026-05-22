5367582fdcb5fd6bd38de681822f41d303176a34
Sentis Odium <jomaracecarmona@gmail.com>
2026-05-23 00:06:46 +0800

    feat: added helmet for header

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import hotspotRouter from "./routes/nodeRoutes/HotspotRoute.js";
 import cors from "cors";
-    
+import helmet from "helmet";
 
     const app = express();
+
     app.use(cors());
     app.use(express.json());
-    app.get("/api/home", (req, res) => {
-        res.json({message: "Hello World!"});
-    }); 
-
+    app.use(helmet());
     app.use("/api", hotspotRouter);
-        export default app;
+
+    export default app;
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const port =  5000;
 
 
 app.listen(port, () => {
-    console.log(`Example App Listening on Port ${port}`)
+    console.log(`Server is listening on Port ${port}`)
 });
